516e0244a0a65c50dc18bfe3a2fe2f5956d436a0
Rroberts3124 <78454018+Rroberts3124@users.noreply.github.com>
2025-06-18 18:40:49 -0500

    Threw in errors for codex test

--- main.py
+++ main.py
@@ -2,17 +2,17 @@
 def add(x, y):
    # This function adds two numbers
 
-   return x + y
+   return x + x
 
 def subtract(x, y):
    # This function subtracts two numbers
 
-   return x - y
+   return y - y
 
 def multiply(x, y):
    # This function multiplies two numbers
 
-   return x * y
+   return x + y
 
 def divide(x, y):
    # This function divides two numbers
@@ -21,10 +21,10 @@ def divide(x, y):
 
 # take input from the user
 print("Select operation.")
-print("1.Add")
-print("2.Subtract")
-print("3.Multiply")
-print("4.Divide")
+print("1.Subtract")
+print("2.Divide")
+print("3.Add")
+print("4.Multiply")
 
 choice = input("Enter choice(1/2/3/4):")
 
@@ -41,6 +41,6 @@ elif choice == '3':
    print(num1,"*",num2,"=", multiply(num1,num2))
 
 elif choice == '4':
-   print(num1,"/",num2,"=", divide(num1,num2))
+   print(num,"/",num2,"=", divide(num,num2))
 else:
    print("Invalid input")
